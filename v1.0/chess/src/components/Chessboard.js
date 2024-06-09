d5f5282a33a796d9cc12ad2811b8b988db034833
Jacob <kuba.kogut.2000@gmail.com>
2024-06-09 11:44:18 +0200

    Zarys planszy
    ...

--- v1.0/chess/src/components/Chessboard.js
+++ v1.0/chess/src/components/Chessboard.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import "../components/Chessboard.css"
 
-const verticalAxis = [1,2,3,4,5,6,7,8];
+const verticalAxis = ["1","2","3","4","5","6","7","8"];
 const horizontalAxis = ["a","b","c","d","e","f","g","h"]
 
 function Chessboard() 
 {
     let board = [];
 
-    for(let i=0; i<horizontalAxis.length; i++)
+    for(let j=verticalAxis.length-1; j>=0; j--)
+    {
+        for(let i=0; i<horizontalAxis.length; i++)
         {
-            for(let j=0; j<verticalAxis; j++)
-                {
-                    board.push(<span>{verticalAxis[j]}{horizontalAxis[i]}</span>)
-                }
-        }
+            const number = j + i + 2;
 
-    return <div id="chessboard">{board[0]}</div>
+            if( number % 2 == 0 ) {
+                board.push(
+                    <div className='color-box'></div>
+                )
+            } else {
+                board.push(
+                    <div className='white-box'></div>
+                )
+            }
+
+        }
+    }
+
+    return (
+    <div id="chessboard">
+        {board}
+    </div>
+    )
 }
 
 export default Chessboard;
